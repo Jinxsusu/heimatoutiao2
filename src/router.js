7e98328608f52f683a3cebecf292e967ba881249
Jinx <Jinx@qq.com>
2019-08-31 19:39:57 +0800

    初始化项目-整理目录-准备资源

--- src/router.js
+++ src/router.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import Home from './views/home/home.vue'// 引入首页
+import Login from './views/login/index.vue'// 引入登录页
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      component: Login
+    }, // 设置登录页
+    {
       path: '/',
+      redirect: '/home'
+    }, // 强制跳转到首页
+    {
+      path: '/home',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
