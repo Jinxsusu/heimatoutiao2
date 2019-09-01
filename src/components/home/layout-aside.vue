c5aafdb2178e7b4a68755d887f56dfad1bab9cc7
Jinx <Jinx@qq.com>
2019-09-01 23:43:06 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,33 +3,40 @@
     <div class="title">
       <img src="../../assets/img/logo_admin.png" alt />
     </div>
-    <el-menu background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
-      <el-menu-item>
+    <el-menu
+      background-color="#323745"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+      :router="true"
+    >
+      <!-- el-menu-item中的index作为路由的path使用 -->
+      <el-menu-item index="/">
         <i class="el-icon-s-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
       <!-- el-submenu 是具有折叠的菜单 -->
       <el-submenu index="1">
+        <!-- el-submenu定义具名插槽 title -->
         <template slot="title">
           <i class="el-icon-s-goods"></i>
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论列表</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论列表</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
       </el-submenu>
       <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-star-on"></i>
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
+        <el-menu-item index="/home/fansdata">图文数据</el-menu-item>
+        <el-menu-item index="/home/fansinfo">粉丝概况</el-menu-item>
+        <el-menu-item index="/home/fanslist">粉丝画像</el-menu-item>
+        <el-menu-item index="/home/fansimg">粉丝列表</el-menu-item>
       </el-submenu>
-      <el-menu-item>
+      <el-menu-item  index="/home/account">
         <i class="el-icon-user-solid"></i>
         <span slot="title">账户信息</span>
       </el-menu-item>
@@ -42,17 +49,17 @@ export default {}
 </script>
 
 <style lang='less' scoped>
-.layout-aside{
-    width: 200px;
-    height: 100vh;
-    background-color: #323745;
-    .title{
-        text-align: center;
-        background-color: #2e2f32;
-        padding: 10px 0;
-        img {
-            height: 30px;
-        }
+.layout-aside {
+  width: 200px;
+  height: 100vh;
+  background-color: #323745;
+  .title {
+    text-align: center;
+    background-color: #2e2f32;
+    padding: 10px 0;
+    img {
+      height: 30px;
     }
+  }
 }
 </style>
